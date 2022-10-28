73dd9e5da64918fc7d6f541b7eee248afdc22af7
Mohamed Elsamadouny <mhsamadouny@gmail.com>
2022-10-28 02:03:39 -0400

    tests for PersonRepository class

---
+++ mmss/src/test/java/ca/mcgill/ecse/mmss/dao/PersonRepositoryTests.java
@@ -0,0 +1,56 @@
+package ca.mcgill.ecse.mmss.dao;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import ca.mcgill.ecse.mmss.model.Person;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+
+public class PersonRepositoryTests {
+  
+  // repository we are testing
+  @Autowired
+  private PersonRepository personRepository;
+  
+  @AfterEach
+  public void clearDatabase() {
+      
+      // empty the person repository
+      personRepository.deleteAll();
+  }
+
+  @Test 
+  public void testPersistAndLoadPerson() { 
+    
+	// create Person
+	Person person = new Person();
+	String firstName = "Test";
+	String lastName = "Person";
+	person.setFirstName(firstName);
+	person.setLastName(lastName);
+	
+	personRepository.save(person);
+	
+	// get person id
+	int personId = person.getPersonId(); 
+    
+    // set person to null
+    person = null;
+    		
+    // get the Person from the repository using the ID
+    person = personRepository.findPersonByPersonId(personId);
+    
+    // run J-Unit tests
+    assertNotNull(person);
+    assertEquals(firstName, person.getFirstName());
+    assertEquals(lastName, person.getLastName()); 
+   
+  }
+}
