357f48ad10afc01198e8075d1cd18b230ac03569
Mohamed Elsamadouny <mhsamadouny@gmail.com>
2022-10-28 02:11:44 -0400

    tests for RoomRepository class

---
+++ mmss/src/test/java/ca/mcgill/ecse/mmss/dao/RoomRepositoryTests.java
@@ -0,0 +1,59 @@
+package ca.mcgill.ecse.mmss.dao;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import ca.mcgill.ecse.mmss.model.Room;
+import ca.mcgill.ecse.mmss.model.Room.RoomType;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+
+public class RoomRepositoryTests {
+  
+  // repository we are testing
+  @Autowired
+  private RoomRepository roomRepository;
+  
+  @AfterEach
+  public void clearDatabase() {
+      
+      // empty the room repository
+      roomRepository.deleteAll();
+  }
+
+  @Test 
+  public void testPersistAndLoadPerson() { 
+    
+	// create Room
+	Room room = new Room();
+	int artefactCount = 20;
+	room.setArtefactCount(artefactCount);
+	room.setRoomType(RoomType.Storage); 
+	roomRepository.save(room); 
+	
+	// Add room to repository
+	roomRepository.save(room);
+	
+	// get room id
+	int roomId = room.getRoomId();
+    
+    // set room to null
+    room = null;
+    		
+    // get the room from the repository using the ID
+    room = roomRepository.findRoomByRoomId(roomId);
+    
+    // run J-Unit tests
+    assertNotNull(room);
+    assertEquals(roomId, room.getRoomId());
+    assertEquals(artefactCount, room.getArtefactCount()); 
+
+   
+  }
+}
