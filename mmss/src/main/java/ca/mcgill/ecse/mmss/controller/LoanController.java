3409aa64a5f3570e8d579f89caba92dda1a0f3e6
Shidan <shidan.javaheri@mail.mcgill.ca>
2022-11-03 22:57:48 -0400

    Intital implementation of createLoan and getLoan in LoanController and LoanService

--- mmss/src/main/java/ca/mcgill/ecse/mmss/controller/LoanController.java
+++ mmss/src/main/java/ca/mcgill/ecse/mmss/controller/LoanController.java
@@ -6,7 +6,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import ca.mcgill.ecse.mmss.model.Loan;
 import ca.mcgill.ecse.mmss.service.LoanService;
@@ -16,11 +22,29 @@ public class LoanController {
     @Autowired
     LoanService loanService;
 
+
     @GetMapping("/loan/{id}")
     public ResponseEntity<Loan> getLoan (@PathVariable int id) { 
         return loanService.retrieveLoanById(id).map(loan -> new ResponseEntity<Loan>(loan, HttpStatus.OK))
         .orElse(new ResponseEntity<Loan>(HttpStatus.NOT_FOUND));
     }
 
-    // @RequestMapping(method = RequestMethod.POST, path = "/loan"); 
+    @RequestMapping (method = RequestMethod.POST, path = "/loan" )
+    public ResponseEntity<Loan> createLoan (@RequestParam String body){ 
+
+    // use the Json parser
+    ObjectMapper mapper = new ObjectMapper();
+
+    try { 
+        Loan loan = mapper.readValue(body, Loan.class); 
+        Loan persistedLoan = loanService.createLoan(loan); 
+        return new ResponseEntity<>(persistedLoan, HttpStatus.CREATED); 
+    } catch (JsonProcessingException e) { 
+        e.printStackTrace();
+    }
+
+    return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+
+    }
+
 }
