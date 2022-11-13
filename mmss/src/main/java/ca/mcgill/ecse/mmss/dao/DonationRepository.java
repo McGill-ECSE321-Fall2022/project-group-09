e61961cf847587f3c6a4619776981b6057496a53
mohamedelsm <85898183+mohamedelsm@users.noreply.github.com>
2022-11-12 22:10:18 -0500

    Revert "Wrote DonationService Class"

--- mmss/src/main/java/ca/mcgill/ecse/mmss/dao/DonationRepository.java
+++ mmss/src/main/java/ca/mcgill/ecse/mmss/dao/DonationRepository.java
@@ -1,22 +1,8 @@
 package ca.mcgill.ecse.mmss.dao;
-import java.sql.Date;
-import java.util.ArrayList;
-
 import org.springframework.data.repository.CrudRepository;
 import ca.mcgill.ecse.mmss.model.Donation;
-import ca.mcgill.ecse.mmss.model.Loan;
-import ca.mcgill.ecse.mmss.model.Visitor;
 
 
 public interface DonationRepository extends CrudRepository<Donation, Integer> {
-    
-	Donation findDonationByExchangeId(int exchangeId);
-    
-	ArrayList<Donation> findAll();
-	
-	ArrayList<Donation> findByVisitor(Visitor visitor);
-
-	ArrayList<Donation> findBySubmittedDate(Date date);
-
-    
+    Donation findDonationByExchangeId(int exchangeId);
 }
