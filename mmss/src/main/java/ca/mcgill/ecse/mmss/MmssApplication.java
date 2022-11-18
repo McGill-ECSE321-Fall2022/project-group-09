1d97497f8cd151450bd0c818447d356760250cd0
Shidan <shidan.javaheri@mail.mcgill.ca>
2022-11-17 20:19:26 -0500

    Set a fixed time zone to stop date change errors

--- mmss/src/main/java/ca/mcgill/ecse/mmss/MmssApplication.java
+++ mmss/src/main/java/ca/mcgill/ecse/mmss/MmssApplication.java
@@ -1,10 +1,20 @@
 package ca.mcgill.ecse.mmss;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MmssApplication {
+
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(MmssApplication.class, args);
