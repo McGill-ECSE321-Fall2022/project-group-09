de9b60a07b9e6348ce162ed73d2c4420bc7a7e6c
Athmane Benarous <atho5850@gmail.com>
2022-11-19 23:34:51 -0500

    Last version including schedule integration

--- mmss/src/test/java/ca/mcgill/ecse/mmss/integration/ScheduleIntegrationTests.java
+++ mmss/src/test/java/ca/mcgill/ecse/mmss/integration/ScheduleIntegrationTests.java
@@ -3,8 +3,6 @@ package ca.mcgill.ecse.mmss.integration;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import java.util.ArrayList;
-
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,22 +10,55 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import ca.mcgill.ecse.mmss.dao.ShiftRepository;
-import ca.mcgill.ecse.mmss.dao.EmployeeRepository;
-import ca.mcgill.ecse.mmss.dao.PersonRepository;
 import ca.mcgill.ecse.mmss.dao.ScheduleRepository;
-import ca.mcgill.ecse.mmss.dto.ShiftDto;
-import ca.mcgill.ecse.mmss.model.Shift;
-import ca.mcgill.ecse.mmss.model.Employee;
-import ca.mcgill.ecse.mmss.model.Person;
+import ca.mcgill.ecse.mmss.dto.ScheduleDto;
 import ca.mcgill.ecse.mmss.model.Schedule;
-import ca.mcgill.ecse.mmss.model.Shift.ShiftTime;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class ScheduleIntegrationTests {
+
+    @Autowired
+    private TestRestTemplate client;
+    @Autowired
+    private ScheduleRepository scheduleRepository;
+
+    Schedule schedule;
+    
+    /**
+     * @author: Athmane Benarous
+     * Create the schedule needed by all the tests
+     */
+    @BeforeEach
+    public void createObjects() {
+        
+        // Create schedule
+        schedule = new Schedule();
+        scheduleRepository.save(schedule);
+    }
+
+    /**
+     * Delete the schedules and the schedules
+     */
+    @AfterEach
+    public void deleteDelete(){
+    	schedule.delete();
+        scheduleRepository.deleteAll();
+    }
+
+    /**
+     * Get the schedule
+     */
+    @Test
+    public void testGetScheduleByScheduleTime() {
+        // Try the get
+        ResponseEntity<ScheduleDto> response = client.getForEntity("/schedule/", ScheduleDto.class);
+        // Assertions
+        assertNotNull(response);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertNotNull(response.getBody());
+        assertEquals(schedule.getScheduleId(), response.getBody().getScheduleId());
+    }
 }
