d77d2765432e58b850e31bfaed7ed08f10588809
Shidan Javaheri <97904673+sjavaheri@users.noreply.github.com>
2022-11-24 20:16:14 -0500

    Started login frontend

---
+++ mmss-frontend/src/components/LoginVisitor.vue
@@ -0,0 +1,42 @@
+<template>
+    <div id="login"> 
+        <h1>
+            Welcome to Marwan's Museum!
+        </h1>
+        <h2>
+            Please login with your account details
+        </h2>  
+
+        <table>
+            <tr>
+                <td>
+                    Username: 
+                </td>
+                <td>
+                    <input type="text" v-model="username" @keydown.space.prevent placeholder="Username">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Password: 
+                </td>
+                <td>
+                    <input type="password" v-model="password" @keydown.space.prevent placeholder="Password">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <button @click="login">Login</button>
+                </td>
+            </tr>
+        </table>
+    </div>
+</template> 
+
+<script>
+</script> 
+
+<style>
+</style>
+
+
--- mmss-frontend/src/router/index.js
+++ mmss-frontend/src/router/index.js
@@ -10,6 +10,9 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    }, 
+    { 
+      
     }
   ]
 })
